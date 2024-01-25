91513489fc30e7184dcd362f603e5cd462f77c7a
Mohamed Saajid <117673036+mohamedsaajid@users.noreply.github.com>
2024-01-25 13:01:36 +0530

    changes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 })
 
-const MONGO_URL = ''; // Enter your MongoDB URL
+const MONGO_URL = ''; // Enter your MongoDB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
